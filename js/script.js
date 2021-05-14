82c394bc9a8af2d03c27ff0dd6d51fe2c53520a9
ZET <63636989+pirilkin@users.noreply.github.com>
2021-05-14 10:59:38 +0500

    14.05.21

--- js/script.js
+++ js/script.js
@@ -4,52 +4,54 @@ map_img_ch.style.width = map_img_unch.offsetWidth + 'px'
 is_clicked = false
 
 function Show_block() {
-    if (is_clicked) {
-        map_img_ch.style.width = map_img_unch.offsetWidth + 'px'
-        map_img_ch.style.transform = 'translate(0,0)';
-        map_img_ch.style.top = 'auto';
-        map_img_ch.style.position = 'absolute'
-        is_clicked = false
-    } else {
-        // Изменить ширину картинки при открытии
-        map_img_ch.style.width = '60%'
+  if (is_clicked) {
+    map_img_ch.style.width = map_img_unch.offsetWidth + 'px'
+    map_img_ch.style.transform = 'translate(0,0)';
+    map_img_ch.style.top = 'auto';
+    map_img_ch.style.position = 'absolute'
+    is_clicked = false
+  } else {
+    // Изменить ширину картинки при открытии
+    map_img_ch.style.width = '60%'
 
-        map_img_ch.style.transform = 'translate(0,-50%)';
-        map_img_ch.style.top = '50%';
-        map_img_ch.style.position = 'fixed'
-        is_clicked = true
-    }
+    map_img_ch.style.transform = 'translate(0,-50%)';
+    map_img_ch.style.top = '50%';
+    map_img_ch.style.position = 'fixed'
+    is_clicked = true
+  }
 }
 //popup
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
+  document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
-    document.getElementById("myForm").style.display = "none";
+  document.getElementById("myForm").style.display = "none";
 }
 //scrolltop
-let topElem = document.querySelector('#top');
+var scroll = $(this).scrollTop();
+var header = $('.scroll').innerHeight();
 
-topElem.onclick = () => backToTop();
-  function backToTop() {
-    if (window.pageYOffset > 0) {
-      window.scrollBy(0, -80);
-      setTimeout(backToTop, 50);
-    }
+if (scroll > header) {
+  $('#top').fadeIn();
+}
+$(window).scroll(function () {
+  var scroll = $(this).scrollTop();
+  var header = $('.scroll').innerHeight();
+  if (scroll > header) {
+    $('#top').fadeIn();
   }
-   function trackScroll() {
-    let scrolled = window.pageYOffset;
-    let coords = document.documentElement.clientHeight;
-    
-    if (scrolled > coords) {
-        console.log('scrolled > coords');
-      topElem.classList.add('back_to_top-show');
-      topElem.classList.remove('back_to_top-hidden');
-    }
-    if (scrolled < coords) {
-      console.log("scrolled < coords");
-      topElem.classList.remove('back_to_top-show');
-      topElem.classList.add('back_to_top-hidden');
-    }
+  else {
+    $('#top').fadeOut();
   }
+})
+$('#top').click(function () {
+  $('html').animate({
+    scrollTop: 0
+  }, 1000)
+
+
+})
+$(window).load(function () {
+  $('.str3-2').liMarquee();
+})
